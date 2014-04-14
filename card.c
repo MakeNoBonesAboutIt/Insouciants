76f5cc1b8110f8a2c8c81c8c9b84c96856d90482
KaloianDoichinov <kaloian_doichinov@abv.bg>
2014-04-14 18:54:47 +0300

    Create card.c

---
+++ card.c
@@ -0,0 +1,32 @@
+#include"card.h"
+#include<stdio.h>
+
+/*Funkciq attack(ne vru6ta realnoto HP sled bitkata)
+Vru6ta 0 ako sa umreli i 2te, 1 ako attacker e jiv,
+2 ako defender e jiv, 3 ako i dvete sa jivi*/
+int attack(struct card_t*attacker, struct card_t*defender)
+{
+	if(attacker->attack >= defender->life)
+		if(defender->attack >= attacker->life)
+			return 0;
+		else
+			return 1;
+	else
+		if(defender->attack >= attacker->life)
+			return 2;
+		else
+			return 3;
+}
+
+/*Glavnata funkciq sus primerni karti*/
+int main()
+{
+	struct card_t card_1, card_2;
+	int result;
+	card_1.attack = 2;
+	card_1.life = 3;
+	card_2.attack = 4;
+	card_2.life = 1;
+	printf("%d\n", result);
+	return 0;
+}
