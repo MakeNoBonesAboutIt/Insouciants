fbf7a3429cb74e188e4189d59c42dfc3766d6452
Vbgf <vasil.v2@mail.bg>
2014-05-11 09:27:32 -0700

    Domashno 4 player.c

---
+++ player.c
@@ -0,0 +1,55 @@
+#include "player.h"
+
+
+int change_hp(struct player_t *player, int hp)
+{
+    if(MAX_HP <= player->player_hp + hp)
+    {
+        player->player_hp = MAX_HP;
+    }
+    else
+    {
+        if(MIN_HP >= player->player_hp + hp)
+        {
+            player->player_hp = MIN_HP;
+            return 1;
+        }
+        else
+        {
+            player->player_hp = player->player_hp + hp;
+        }
+    }
+    return 0;
+}
+
+struct card_t play_card_from_hand(struct player_t *player, int card)
+{
+    int n = player->player_hand.n;
+    int i;
+    struct card_t result;
+    if(n <= card || card < 0)
+    {
+        result.attack = 0;
+        result.life = 0;
+        result.cost = 0;
+        result.name[0] = '\0';
+    }
+    else
+    {
+        result = player->player_hand.cards[card];
+        for(i = card + 1; i < n; i++)
+        {
+            player->player_hand.cards[i-1] = player->player_hand.cards[i];
+        }
+        player->player_hand.n = n - 1;
+    }
+    return result;
+}
+
+void turn_begin(struct player_t *player)
+{
+    struct card_t card;
+    player->player_mana.mana ++;
+    if(draw_card(&player->player_deck, &card) == 0)
+        push_card(card, &player->player_hand);
+}
